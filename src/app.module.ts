8f4089605eafe623ca5b2a5dc48a7ec1f1b00a1e
Roman Pavtso <romkalily@gmail.com>
2021-03-06 01:52:17 +0200

    version without common db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { EModule } from './eCrud/e.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [EModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
