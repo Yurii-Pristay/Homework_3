c45f79eaf5c89c514051179944ed4eafb671312a
Roman Pavtso <romkalily@gmail.com>
2021-03-07 19:52:47 +0200

    E connected to dbArray

--- src/eCrud/e.controller.ts
+++ src/eCrud/e.controller.ts
@@ -1,3 +1,4 @@
+import { DbObject } from './../interfaces/DbObject';
 import { CreateEDto } from './dto/create-e.dto';
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, Query } from "@nestjs/common";
 import { UpdateEDto } from './dto/update-e.dto';
@@ -10,23 +11,23 @@ export class EController {
 
 
     @Get()
-    get(@Query('persantage') percentage) {
+    get(@Query('percentage') percentage: boolean): DbObject[] | string {
         return this.eService.get(percentage)
     }
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
-    create(@Body() createE: CreateEDto) {  
+    create(@Body() createE: CreateEDto): DbObject {  
         return this.eService.create(createE)
     }
 
     @Put(':id') 
-    update(@Body() updateE: UpdateEDto, @Param('id', ParseIntPipe) id: string) {
+    update(@Body() updateE: UpdateEDto, @Param('id', ParseIntPipe) id: number): DbObject {
         return this.eService.update(updateE,id)
     }
 
     @Delete(':id')
-    delete(@Param('id', ParseIntPipe) id: string) {
+    delete(@Param('id', ParseIntPipe) id: number): string {
         return this.eService.delete(id)
     }
 
--- src/eCrud/e.service.ts
+++ src/eCrud/e.service.ts
@@ -1,6 +1,8 @@
+import { DbObject } from './../interfaces/DbObject';
 import { CreateEDto } from './dto/create-e.dto';
 import { Injectable } from "@nestjs/common";
 import { UpdateEDto } from './dto/update-e.dto';
+import { DatabaseService } from 'src/database/database.service';
 
 function getRandomInt(min: number, max: number): number {
     min = Math.ceil(min);
@@ -10,47 +12,42 @@ function getRandomInt(min: number, max: number): number {
 
 @Injectable()
 export class EService {
-    private dbArray = [
-        { id: 1, a: [1, 2, 3] },
-        { id: 2, b: { key: "test" } },
-        { id: 3, c: 123 },
-        { id: 4, d: "test" },
-        { id: 5, e: true },
-    ];
+    constructor(private dbService: DatabaseService) {}
 
-    get(percentage) {
-        if(percentage)//use queryparammmmm for get percentage
+    get(percentage: boolean): DbObject[]|string {//use queryparammmmm for get percentage
+        if(percentage)
         {
-            let percent = this.dbArray.filter(item => item.e === true).length / this.dbArray.filter(item => item.e === false).length;
+            let percent = this.dbService.dbArray.filter(item => item.e === true).length / this.dbService.dbArray.filter(item => item.e === false).length;
             return percent*100 + "% true to false"
         }
-        return this.dbArray.filter(item => item.e);
+        return this.dbService.dbArray.filter(item => item.e);
     }
 
-    create(eDto: CreateEDto) {
-        const newId: number = Math.max(...this.dbArray.map(({ id }) => id)) + 1;
-        const createObj = {id: newId, e: eDto.e}
-        this.dbArray.push(createObj);
-        return this.dbArray[getRandomInt(1, this.dbArray.length)]//return random obj
+    create(eDto: CreateEDto): DbObject {
+        const newId: number = Math.max(...this.dbService.dbArray.map(({ id }) => id)) + 1;
+        const createObj: DbObject = {id: newId, e: eDto.e}
+        this.dbService.dbArray.push(createObj);
+        return this.dbService.dbArray[getRandomInt(1, this.dbService.dbArray.length)]//return random obj
     }
 
-    update(eDto: UpdateEDto, id) {
-        let updateObj;
-        this.dbArray.map((item) => {
+    update(eDto: UpdateEDto, id: number): DbObject {
+        let updateObj: DbObject;
+        this.dbService.dbArray.map((item) => {
             if (item.id === id) {
                 item.e = eDto.e;
                 updateObj = item;
             }
         });
-        return this.dbArray;
+        return updateObj;
     }
 
-    delete(idToRemove) {
-        const indToRemove = this.dbArray.findIndex((item) => item.id === idToRemove);//add here parseintpipe
+    delete(idToRemove: number): string {
+        const indToRemove = this.dbService.dbArray.findIndex((item) => item.id === idToRemove);//add here parseintpipe
         if (indToRemove === -1) {
-            return `Element with id ${idToRemove} wasn't found`;
+            return `Element with id ${idToRemove} wasn't found(`;
         }
-        this.dbArray.splice(indToRemove, 1);
+        this.dbService.dbArray.splice(indToRemove, 1);
+        return `Element with id ${idToRemove} was deleted)`
     }
 }
 
