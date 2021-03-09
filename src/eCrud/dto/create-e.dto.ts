748a1db2e6d6166465125bf19b59540f796befff
Roman Pavtso <romkalily@gmail.com>
2021-03-09 21:35:42 +0200

    use whitelist in ecrud

--- src/eCrud/dto/create-e.dto.ts
+++ src/eCrud/dto/create-e.dto.ts
@@ -1,3 +1,5 @@
+import { IsBoolean} from 'class-validator'
 export class CreateEDto {
-    readonly e: boolean
+    @IsBoolean()
+    e: boolean
 }
--- src/eCrud/dto/update-e.dto.ts
+++ src/eCrud/dto/update-e.dto.ts
@@ -1,3 +1,6 @@
+import { IsBoolean } from "class-validator";
+
 export class UpdateEDto {
-    readonly e: boolean
+    @IsBoolean()
+    e: boolean
 }
