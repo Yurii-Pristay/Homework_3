beda405fab21430b099d733a3cb030aee08f2529
NastiiaV <awerga13@gmail.com>
2021-04-28 13:20:24 +0300

    connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,12 +13,13 @@ import { E } from './eCrud/e.entity';
 @Module({
   imports: [EModule, DModule,CCrudModule, DatabaseModule,
     TypeOrmModule.forRoot({
+      //url: 'postgres://yitlizsstnirrj:b6b05cdb2992521ad8422dc65981905df22f7eaeb3913187c46293077b63d416@ec2-3-217-219-146.compute-1.amazonaws.com:5432/d22a970h6gmbj2',
       type: 'postgres',
-      host:process.env.POSTGRES_HOST || 'localhost',
+      host:process.env.POSTGRES_HOST || 'localhost' , //localhost
       port: 5432,
-      username:process.env.POSTGRES_USERNAME || 'postgres',
-      password:process.env.POSTGRES_PASSWORD || 'pgadmin2021',         
-      database:process.env.POSTGRES_DATABASE || 'routesdb',
+      username:process.env.POSTGRES_USERNAME ||'postgres' , //'postgres'
+      password:process.env.POSTGRES_PASSWORD || 'pgadmin2021' ,//' 'pgadmin2021',         
+      database:process.env.POSTGRES_DATABASE ||'routesdb', // 'routesdb',
       entities:[RouteС, E],
       autoLoadEntities: true,
       synchronize: true,
